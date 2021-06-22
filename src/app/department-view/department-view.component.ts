51a639119896aa8b6105b89ec3f23f6e6fe533a3
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2021-06-22 21:10:47 +0530

    dept list done

--- src/app/department-view/department-view.component.ts
+++ src/app/department-view/department-view.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HospitalService } from '../hospital.service';
+import { ValidationService } from '../validation.service';
 @Component({
   selector: 'app-department-view',
   templateUrl: './department-view.component.html',
-  styleUrls: ['./department-view.component.scss']
+  styleUrls: ['./department-view.component.scss'],
+  providers: [HospitalService, ValidationService]
 })
-export class DepartmentViewComponent implements OnInit {
+export class DepartmentViewComponent implements OnInit, OnDestroy {
+  departmentList: any = [];
+  departmentDataList$;
+  formSubmitted = false;
+  isEdit = false;
+  
+  departmentObj = {
+    departmentname: '',
+    head : '',
+    contactnumber: '',
+    hospitalId: '',
+    hospitalname: ''
+  };
 
-  constructor() { }
+  constructor(
+    private hospitalSvc: HospitalService,
+    private validator: ValidationService
+  ) { }
 
   ngOnInit() {
+    this.getAllDepts();
   }
+
+  getAllDepts() {
+    this.departmentDataList$ = this.hospitalSvc.getAllDept().subscribe((res) => {
+      this.departmentList = res;
+    });
+  }
+
+  editDept(dept) {
+    alert('edit operation is not implemented');
+  }
+
+  deleteDept(dept) {
+    alert('delete operation is not implemented');
+  }
+
+  ngOnDestroy() {
+    this.departmentDataList$.unsubscribe();
+  }
+
 
 }
